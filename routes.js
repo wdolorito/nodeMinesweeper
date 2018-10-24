b22ba0bb465d6c3f2684eb81aca12f8534a3be3b
William Dolorito <wdolorito@gmail.com>
2018-10-24 07:58:43 -0700

    Added gmail-send for modal form use

--- routes.js
+++ routes.js
@@ -2,6 +2,13 @@ const express = require('express');
 const router = express.Router();
 const views = __dirname + '/views'
 
+const credentials = require(__dirname + '/gmail-credentials.json')
+const send = require('gmail-send')({
+  user: credentials.user,
+  pass: credentials.pass,
+  to:   credentials.user,
+})
+
 router.use(function (req, res, next) {
   console.log(Date() + ' ' + req.path + ' ' + req.method)
   next()
